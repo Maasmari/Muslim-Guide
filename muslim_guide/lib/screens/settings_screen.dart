a48e775c693b82e62f7a11398be15813c41ea050
Hoby21 <hobylobytoby@gmail.com>
2024-01-28 19:31:03 +0300

    Settings screen (WIP)
    updated some visuals

--- muslim_guide/lib/screens/settings_screen.dart
+++ muslim_guide/lib/screens/settings_screen.dart
@@ -17,53 +17,62 @@ class _SettingsState extends State<SettingsScreen> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Settings'),
+          title: const Text('Settings', style: TextStyle(color: Colors.white),),
           centerTitle: true,
           backgroundColor: const Color.fromARGB(255, 30, 87, 32),
         ),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                const Text('Dark mode'),
-                const Spacer(),
-                Switch(
-                  value: isSwitched,
-                  onChanged: (value) { //need to make light theme and dark theme for dark/light mode
-                    setState(
-                      () {
-                        isSwitched = value;
-                      },
-                    );
-                  },
-                )
-              ],
-            ),
-            const Row(
-              children: [
-                Text('Send feedback'),
-                Spacer(),
-                Icon(Icons.arrow_right),
-              ],
-            ),
-            const Row(
-              children: [
-                Text('Profile'),
-                Spacer(),
-                Icon(Icons.arrow_right),
-              ],
-            ),
-            const Row(
-              children: [
-                Text(
-                  'Logout',
-                  style: TextStyle(color: Color.fromARGB(255, 202, 13, 0)),
-                ),
-                Spacer(),
-                Icon(Icons.exit_to_app),
-              ],
-            ),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const Text('Dark mode'),
+                  const Spacer(),
+                  Switch(
+                    value: isSwitched,
+                    onChanged: (value) { //need to make light theme and dark theme for dark/light mode
+                      setState(
+                        () {
+                          isSwitched = value;
+                        },
+                      );
+                    },
+                  )
+                ],
+              ),
+              const Divider(color: Colors.black),
+              const SizedBox(height: 6),
+              const Row(
+                children: [
+                  Text('Send feedback'),
+                  Spacer(),
+                  Icon(Icons.arrow_right),
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Divider(color: Colors.black),
+              const Row(
+                children: [
+                  Text('Profile'),
+                  Spacer(),
+                  Icon(Icons.arrow_right),
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Divider(color: Colors.black),
+              const Row(
+                children: [
+                  Text(
+                    'Logout',
+                    style: TextStyle(color: Color.fromARGB(255, 202, 13, 0)),
+                  ),
+                  Spacer(),
+                  Icon(Icons.exit_to_app),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
