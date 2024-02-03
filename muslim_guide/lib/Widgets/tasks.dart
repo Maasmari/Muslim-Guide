289be90475772bfc2f04c02fe9617b77a9032e93
Hoby21 <hobylobytoby@gmail.com>
2024-02-03 20:08:14 +0300

    updated tasks.dart

--- muslim_guide/lib/Widgets/tasks.dart
+++ muslim_guide/lib/Widgets/tasks.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:muslim_guide/Widgets/tasks_list.dart';
 import 'package:muslim_guide/models/task.dart';
 
 class Tasks extends StatefulWidget {
@@ -12,15 +13,31 @@ class Tasks extends StatefulWidget {
 
 class _TasksState extends State<Tasks> {
   final List<Task> _registeredTasks = [
-    Task(taskName: 'Surat Al Kahf', taskDescription: 'Read Surat Al Kahf.', taskType: TaskType.optional, taskFrequency: TaskFrequency.once)
+    Task(
+        taskName: 'Surat Al Kahf',
+        taskDescription: 'Read Surat Al Kahf.',
+        taskType: TaskType.optional,
+        taskFrequency: TaskFrequency.once),
+    Task(
+        taskName: 'Read Dua',
+        taskDescription: 'Don\'t forget to read dua!',
+        taskType: TaskType.optional,
+        taskFrequency: TaskFrequency.daily),
+    Task(
+        taskName: 'Fasting',
+        taskDescription: 'Do not drink or eat until Adhaan Al Maghreb!',
+        taskType: TaskType.optional,
+        taskFrequency: TaskFrequency.monthly)
   ];
 
   @override
   Widget build(context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          
+          Expanded(
+            child: TasksList(tasks: _registeredTasks),
+          ),
         ],
       ),
     );
